580f7245b79d3aafd449b60fce0d5eb7bc1afe41
Silas Maciel <silas-ferreira@hotmail.com>
2017-09-30 22:44:38 -0300

    Primeiro commit

---
+++ app/src/main/java/br/com/silas/letusknow/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package br.com.silas.letusknow.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.silas.letusknow.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
---
+++ app/src/main/java/br/com/silas/letusknow/activity/SplashActivity.java
@@ -0,0 +1,30 @@
+package br.com.silas.letusknow.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import br.com.silas.letusknow.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handle = new Handler();
+        handle.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+        }, 2000);
+    }
+
+}
