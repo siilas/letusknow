9e339f4a07cba2351b3163ae808200f626e0a7b2
Silas Maciel <silas-ferreira@hotmail.com>
2017-10-29 14:31:13 -0200

    Tratamento de erros

--- app/src/main/java/br/com/silas/letusknow/activity/BaseActivity.java
+++ app/src/main/java/br/com/silas/letusknow/activity/BaseActivity.java
@@ -1,18 +1,40 @@
 package br.com.silas.letusknow.activity;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import br.com.silas.letusknow.R;
 import br.com.silas.letusknow.exception.ServiceException;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
     protected void mostarMensagemErro(String mensagem, Exception erro) {
         Log.e("LetUsKnow", mensagem, erro);
+        mensagemErro(mensagem);
     }
 
     protected void mostarMensagemErro(ServiceException erro) {
+        mensagemErro(erro.getMessage());
+    }
 
+    private void mensagemErro(String erro) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(erro);
+        builder.setCancelable(false);
+
+        builder.setPositiveButton(
+                R.string.error_button,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                        finish();
+                    }
+                });
+
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 
 }
